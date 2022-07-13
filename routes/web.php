b2dc6c72adc33a71d6c452c0d445417e8160d8d8
Wahyu Rochman <wrochcode@gmail.com>
2022-07-13 17:00:07 +0700

    update route

--- routes/web.php
+++ routes/web.php
@@ -3,7 +3,6 @@
 use App\Http\Controllers\AboutController;
 use App\Http\Controllers\ArticleController;
 use App\Http\Controllers\HomeController;
-use App\Http\Controllers\LoginController;
 use App\Http\Controllers\LogoutController;
 use App\Http\Controllers\MasukController;
 use App\Http\Controllers\ProductController;
